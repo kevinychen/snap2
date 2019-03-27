9731e15cfca28ed527ae667858ef3c106000ff1c
Kevin Y. Chen <kchen@palantir.com>
2019-03-26 17:40:59 -0700

    Levenschtein distance

--- snap-server/src/main/java/com/kyc/snap/words/StringUtil.java
+++ snap-server/src/main/java/com/kyc/snap/words/StringUtil.java
@@ -100,6 +100,26 @@ public class StringUtil {
         return low;
     }
 
+    /**
+     * Find the Levenshtein distance between a and b.
+     * https://en.wikipedia.org/wiki/Levenshtein_distance
+     */
+    public static int levenshteinDistance(String a, String b) {
+        int[] costs = new int[b.length() + 1];
+        for (int j = 0; j <= b.length(); j++)
+            costs[j] = j;
+        for (int i = 1; i <= a.length(); i++) {
+            costs[0] = i;
+            int nw = i - 1;
+            for (int j = 1; j <= b.length(); j++) {
+                int cj = Math.min(1 + Math.min(costs[j], costs[j - 1]), a.charAt(i - 1) == b.charAt(j - 1) ? nw : nw + 1);
+                nw = costs[j];
+                costs[j] = cj;
+            }
+        }
+        return costs[b.length()];
+    }
+
     private StringUtil() {
     }
 }
