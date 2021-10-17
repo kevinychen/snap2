98b28c1681d75be122c6d3cb7de7484700e6effc
Kevin Y. Chen <kyc2915@gmail.com>
2021-10-16 17:17:18 -0700

    UI v2 with React

---
+++ snap-app/src/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Redirect,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
+import Parser from './parser';
+import './index.css';
+
+function Index() {
+    return <>
+        <div className="title">
+            <Link to="/" className="hsection"><img src="./wrench.png" alt="Home" /></Link>
+            util.in
+        </div>
+        <div className="vsection">
+            <Link to="/parser"><span className="link">GRID PARSER</span></Link>
+            <br />
+            Parse grids, text, and crosswords in PDFs and images to Google Sheets
+        </div>
+    </>;
+}
+
+function Header() {
+    return <div class="vsection">
+        <Link to="/" className="hsection"><img src="./wrench.png" alt="Home" /></Link>
+        <Link to="/parser"><span className="hsection link">GRID PARSER</span></Link>
+    </div>;
+}
+
+function App() {
+    return <Router>
+        <Switch>
+            <Route path="/parser">
+                <Header />
+                <Parser />
+            </Route>
+            <Route path="/">
+                <Index />
+            </Route>
+            <Route path="/index.html">
+                <Redirect to="/" />
+            </Route>
+            <Route path="/document.html">
+                <Redirect to="/parser" />
+            </Route>
+        </Switch>
+    </Router>;
+}
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
---
+++ snap-app/src/parser/parser.js
@@ -0,0 +1,3 @@
+export default function Parser() {
+    return "hi";
+}
